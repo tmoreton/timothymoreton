9d25bfcad770566571ce81f4d8001108a5b99a75
Tim Moreton <tmoreton89@gmail.com>
2023-12-21 13:07:21 -0300

    update

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -3,13 +3,11 @@ import { getDocumentBySlug, getDocuments } from 'outstatic/server'
 
 export default async function Index({ params }: { params: any }) {
   const post = await getPost(params)
+  return <div>{post?.title}</div>
   return <ArticleLayout article={post} />
 }
 
 async function getPost(params: any) {
-  console.log(params)
-  // let articles = getDocuments('posts', ['title', 'slug', 'publishedAt', 'description', 'content', 'description', 'coverImage'])
-  // let article = articles.find(a => a.slug === params.slug)
   let article = getDocumentBySlug('posts', params?.slug, [
     'title',
     'publishedAt',
