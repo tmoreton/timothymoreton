74667ebb481b36c512340626127d530a998f720d
Tim Moreton <tmoreton89@gmail.com>
2023-12-21 12:50:00 -0300

    try other method

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -8,17 +8,17 @@ export default async function Index({ params }: { params: any }) {
 
 async function getPost(params: any) {
   console.log(params)
-  // let articles = getDocuments('posts', ['title', 'slug', 'publishedAt', 'description', 'content', 'description', 'coverImage'])
-  // let article = articles.find(a => a.slug === params.slug)
-  let article = getDocumentBySlug('posts', params.slug, [
-    'title',
-    'publishedAt',
-    'slug',
-    'author',
-    'content',
-    'coverImage',
-    'description'
-  ])
+  let articles = getDocuments('posts', ['title', 'slug', 'publishedAt', 'description', 'content', 'description', 'coverImage'])
+  let article = articles.find(a => a.slug === params.slug)
+  // let article = getDocumentBySlug('posts', params.slug, [
+  //   'title',
+  //   'publishedAt',
+  //   'slug',
+  //   'author',
+  //   'content',
+  //   'coverImage',
+  //   'description'
+  // ])
   console.log(article)
   return article
 }
