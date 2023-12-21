ab9165e1a7eca85b4ca2c70feb12feff908a4f3c
Tim Moreton <tmoreton89@gmail.com>
2023-12-21 14:40:15 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { type Metadata } from 'next'
-
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
 
@@ -7,12 +6,12 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - Timothy Moreton',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      'Timothy Moreton - Software engineer, founder, and avid traveler',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I’m Tim, a software engineer and entrepreneur based in Miami, FL. I’m the founder of Trckfi, where we help people achieve financial clarity through our all-in-one money dashboard',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
