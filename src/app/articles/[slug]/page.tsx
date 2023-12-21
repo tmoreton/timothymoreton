fe6220d174c812b1667d2ea39ffd2a669598936d
Tim Moreton <tmoreton89@gmail.com>
2023-12-21 12:18:36 -0300

    read posts from outstatic

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -3,10 +3,12 @@ import { getDocumentBySlug } from 'outstatic/server'
 
 export default async function Index({ params }: { params: any }) {
   const post = await getPost(params)
+  console.log(post)
   return <ArticleLayout article={post} />
 }
 
 async function getPost(params: any) {
+  console.log(params)
   return getDocumentBySlug('posts', params.slug, [
     'title',
     'publishedAt',
