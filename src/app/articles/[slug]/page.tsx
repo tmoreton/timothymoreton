d8ce1c30f82a9cbf9e69d224f07d99b1910de5c9
Tim Moreton <tmoreton89@gmail.com>
2023-12-21 12:37:09 -0300

    try other method

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,14 +1,17 @@
 import { ArticleLayout } from '@/components/ArticleLayout'
-import { getDocumentBySlug } from 'outstatic/server'
+import { getDocumentBySlug, getDocuments } from 'outstatic/server'
 
 export default async function Index({ params }: { params: any }) {
   const post = await getPost(params)
-  console.log(post)
   return <ArticleLayout article={post} />
 }
 
 async function getPost(params: any) {
   console.log(params)
+  let articles = getDocuments('posts', ['title', 'slug', 'publishedAt', 'description'])
+  let article = articles.find(a => a.slug === params.slug)
+  console.log(article)
+  return article
   return getDocumentBySlug('posts', params.slug, [
     'title',
     'publishedAt',
