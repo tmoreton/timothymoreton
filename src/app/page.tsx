4ca1d2a060258fb41b27149a7b4d1b2507fb4e86
Tim Moreton <tmoreton89@gmail.com>
2024-01-03 15:41:10 -0300

    add footer input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,8 +114,9 @@ function SocialLink({
 function Newsletter() {
   return (
     <form
-      action="/thank-you"
+      action="https://rewrite.blog/email/subscribe"
       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
+      method="get"
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <MailIcon className="h-6 w-6 flex-none" />
@@ -132,6 +133,8 @@ function Newsletter() {
           required
           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
         />
+        <input hidden name="token" type="text" value="clqwi1gvi0001ax9ld32fyusg" />
+        <input hidden name="blog" type="text" value="timothymoreton" />
         <Button type="submit" className="ml-4 flex-none">
           Join
         </Button>
@@ -319,7 +322,7 @@ export default async function Home() {
             ))}
           </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
-            {/* <Newsletter /> */}
+            <Newsletter />
             <Resume />
           </div>
         </div>
